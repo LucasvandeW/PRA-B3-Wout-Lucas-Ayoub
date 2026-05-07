bc149989f887898ebf3fd7ff4a5c59ccecb791c7
woutk033290 <woutkerbusch@gmail.com>
2026-05-07 10:55:06 +0200

    bezig met header

--- resources/views/components/header.php
+++ resources/views/components/header.php
@@ -1,1 +1,21 @@
 <?php #WORK IN PROGRESS ?>
+<head>
+    <link rel="stylesheet" href="../css/header.css">
+</head>   
+
+
+<header>
+        <div class="logo">   <img src="../img/logo-big-v4.png" alt="Developerland Logo">  </div>
+
+
+
+        <div class="nav-buttons">
+            <button><a href="../index.php">Terug naar home &gt;</a></button>
+             <button><a href="create.php">Taak aanmaken &gt;</a></button>
+             <button><a href="done.php">Uitgevoerde taken &gt;</a></button>
+             <button><a href="mytasks.php">Bekijk mijn taken &gt;</a></button>
+            <button><a href="categories.php">Bekijk taken per afdeling &gt;</a></button>
+
+        </div>
+        
+    </header>
